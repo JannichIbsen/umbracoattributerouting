985099dc0189d33bd37e7d943b50d3e7c6c0a432
KVK\JAIB <jannich.ibsen@kraftvaerk.com>
2018-12-06 13:59:32 +0100

    Initial commit

---
+++ UmbRoutingTest/ApiControllers/ValueController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Umbraco.Web.WebApi;
+
+namespace UmbRoutingTest.ApiControllers
+{
+    [RoutePrefix("api/value")]
+    public class ValueController : UmbracoApiController
+    {
+
+        [Route(Name = "GetValues")]
+        public FormattedContentResult<string> Get()
+        {
+            return base.Content(HttpStatusCode.OK, "Content", new JsonMediaTypeFormatter());
+        }
+    }
+}
---
+++ UmbRoutingTest/Events/AppEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Core;
+
+namespace UmbRoutingTest.Events
+{
+    public class AppEventHandler : ApplicationEventHandler
+    {
+
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            GlobalConfiguration.Configuration.MapHttpAttributeRoutes();
+        }
+    }
+}
